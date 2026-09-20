d2f04716698d35f64c0c1b4a5455b3139974f668
HAMMAM TAHA <hamamhmam72@gmail.com>
2026-09-20 19:34:20 +0300

    Refactor pino-http import and middleware setup

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -8,6 +8,25 @@ const app: Express = express();
 
 app.use(
   pinoHttp({
+    app.use(
+  pinoHttp.default({
+    logger,
+    serializers: {
+      req(req) {
+        return {
+          id: req.id,
+          method: req.method,
+          url: req.url?.split("?")[0],
+        };
+      },
+      res(res) {
+        return {
+          statusCode: res.statusCode,
+        };
+      },
+    },
+  })
+);
     logger,
     serializers: {
       req(req) {
