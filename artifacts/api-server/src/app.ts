e5fe01a79a0b8d3233e93a9ae650716f5c93336a
HAMMAM TAHA <hamamhmam72@gmail.com>
2026-09-20 19:46:59 +0300

    Refactor HTTP logger setup in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -6,25 +6,25 @@ import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
+const httpLogger = pinoHttp({
+  logger,
+  serializers: {
+    req(req) {
+      return {
+        id: req.id,
+        method: req.method,
+        url: req.url?.split("?")[0],
+      };
     },
-  })
-);
+    res(res) {
+      return {
+        statusCode: res.statusCode,
+      };
+    },
+  },
+});
+
+app.use(httpLogger);
 
 app.use(cors());
 app.use(express.json());
