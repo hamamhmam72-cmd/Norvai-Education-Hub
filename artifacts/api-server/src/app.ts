a59aa5cc72460d1a979f96ca974993a89afd2f3b
HAMMAM TAHA <hamamhmam72@gmail.com>
2026-09-21 13:00:07 +0300

    Fix import and usage of pino-http middleware

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,12 +1,12 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
 
-app.use(pinoHttp({ logger }));
+app.use(pinoHttp.default({ logger }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
